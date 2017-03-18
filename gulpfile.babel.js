6b91f9f21d1274fd74f63e686a23b8cd08f50938
Fabio <fabio@Fabio-Damasceno.local>
2017-03-18 11:33:13 -0300

    adicionada task de psr

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,11 +3,19 @@ import help from 'gulp-help'
 
 const gulp = help(Gulp)
 
-gulp.task('default', ['help'], () => {})
+gulp.task('default', ['help'])
 
 gulp.task('php', 'Move arquivos php', () => {
 	gulp.src('app/**/*.*')
 		.pipe(gulp.dest('wordpress/wp-content'))
 })
 
-gulp.task('build', 'Build da aplicação', ['php'], () => {})
+gulp.task('ativacao:wordpress', 'php', ()=>{
+
+})
+
+gulp.task('psr', 'verifica namespaces e valida code style', ()=>{
+
+})
+
+gulp.task('build', 'Build da aplicação', ['ativacao:wordpress'], () => {})

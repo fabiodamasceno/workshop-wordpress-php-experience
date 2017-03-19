f85c5566a9326756db5cce4d5d8d1c9eab014b13
fabiodamasceno <fabio@Fabio-Damasceno.local>
2017-03-19 14:23:06 -0300

    removidas senhas

--- deployment/logger.js
+++ deployment/logger.js
@@ -3,8 +3,8 @@ import colors from 'colors'
 export default function() {
     console.error('Formato incorreto!!'.red);
     console.info('Parâmetros:'.yellow)
-    console.info('--usuario $siteinstitucionalicatu'.grey)
-    console.info('--senha aFGAxSrPxCAqsyDHCxY2pdDB2FBF4esfsXuS8nv56Q7zZhjyuzL2hH7Tt9jK'.grey)
-    console.info('--host https://siteinstitucionalicatu.scm.azurewebsites.net/ (opcional)'.grey)
+    console.info('--usuario'.grey)
+    console.info('--senha'.grey)
+    console.info('--host (opcional)'.grey)
     process.exit(1);
 }

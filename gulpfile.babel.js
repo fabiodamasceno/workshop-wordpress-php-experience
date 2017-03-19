5be081ea356517e74b34d68e57eed511110860d7
fabiodamasceno <fabio@Fabio-Damasceno.local>
2017-03-19 13:00:32 -0300

    removida task desnecessária de ativaçao:wordpress do gulp, ajuste da ordem npm start

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,16 +6,14 @@ const gulp = help(Gulp)
 
 gulp.task('default', ['help'])
 
-gulp.task('php', 'Move arquivos php', () => {
-    gulp.src('app/**/*.*')
-        .pipe(gulp.dest('wordpress/wp-content'))
-})
-
-gulp.task('ativacao:wordpress', 'php', () => {})
-
 gulp.task('psr', 'verifica namespaces e valida code style', () => {})
 
-gulp.task('build', 'Build da aplicação', ['ativacao:wordpress'], () => {})
+gulp.task('php', 'Move arquivos php', ['psr'], () => {
+    return gulp.src('./app/**/*.*')
+        .pipe(gulp.dest('./wordpress/wp-content/'))
+})
+
+gulp.task('build', 'Build da aplicação', ['php'], () => {})
 
 gulp.task('deploy', 'Deploy da aplicação', () => {
     deploy()

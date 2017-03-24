f921f68b9b3d5dd26bf8d9d85fa882cbf512c876
Bruno Passos <brunoalmeidapassos@gmail.com>
2017-03-24 16:35:02 -0300

    Hello dolly (#4)
    * Plugin de teste que exibe trechos da música Hello Dolly no wordpress
    
    * Adicionado phpunit como dependência do composer
    
    Para rodar os testes unitários: vendor/bin/phpunit app
    
    * Instala as dependências do composer no npm run start

---
+++ app/plugins/HelloDolly/HelloDollyTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Plugins\HelloDolly;
+
+class HelloDollyTest extends \PHPUnit_Framework_TestCase
+{
+    private $musica;
+
+    public function setUp()
+    {
+        $this->musica = ["Hello, Dolly"];
+    }
+
+    public function testDeveRetornarSomenteUmaFraseDaMúsicaQuandoCharmOMetodo()
+    {
+        $helloDolly = new HelloDolly($this->musica);
+        $resultado = $helloDolly->obterFrase();
+
+        $this->assertEquals("Hello, Dolly", $resultado);
+    }
+
+    public function testDeveExibirFraseNaTelaQuandoChamarExibirFrase()
+    {
+        $template = "<p>{{letra}}</p>";
+        $helloDolly = new HelloDolly($this->musica, $template);
+
+        $this->expectOutputString("<p>Hello, Dolly</p>", $helloDolly->exibirFrase());
+    }
+}
+

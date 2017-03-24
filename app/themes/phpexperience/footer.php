de32852b962d872ea6f312441bf5007101a9ffdb
Fabio Damasceno <fabio.damasceno@lambda3.com.br>
2017-03-23 23:37:12 -0300

    tema atualizado com informações de deployment

--- app/themes/phpexperience/footer.php
+++ app/themes/phpexperience/footer.php
@@ -4,23 +4,13 @@
 	<script>
 	if (!window.jQuery) 
 	{
-	    document.write('<script src="js/vendor/jquery.1.11.min.js"><\/script>');
-	}
-	</script>
+	    document.write('<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/vendor/jquery.1.11.min.js"><\/script>');
+    }
+    </script>
 
-	<script src="js/vendor/jquery.fluidbox.min.js"></script>
-	<script src="js/main.js"></script>
-
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'YOUR_GOOGLE_ANALYTICS_ID', 'auto');
-	  ga('send', 'pageview');
-
-	</script>
-
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/vendor/jquery.fluidbox.min.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/main.js"></script>
 </body>
 </html>
+<?php wp_footer() ?>
+<?php ?>
--- app/themes/phpexperience/index.php
+++ app/themes/phpexperience/index.php
@@ -1,46 +1,15 @@
 <?php get_header(); ?>
 <div id="content">
         <section class="intro">
-            <h1>Hello, <span class="nl"></span>we are here to help you.</h1>
+            <h1>Olá, <span class="nl"></span> nós entregamos em produção :D</h1>
             <p>
-                Gumba is a simple single-page website template. This is the place where you would normally introduce yourself. You can easily change the template to fit your needs by changing the text, photos and styles.
+                Esta é a forma que nós encontramos de realizar entrega contínua mais simples possível. Agora evolua e nos mostre a sua forma! 
             </p>
         </section>
-
-        <section class="row">
-            <div class="col-full">
-                <h2>Example title</h2>
-                <p>
-                    Gumba is a fictional agency from Amsterdam, The Netherlands. This is the place where you would normally introduce yourself. You can easily change the template to fit your needs by adding text or changing the colors and styles.
-                </p>
-            </div>
-        </section>
-
-        <section class="row">
-            <div class="photo-grid">
-                <a href="img/example-photo-b.jpg" rel="lightbox" class="col-2"><img src="img/example-photo-b.jpg" alt="Example photo"></a>
-                <a href="img/example-photo-c.jpg" rel="lightbox" class="col-2"><img src="img/example-photo-c.jpg" alt="Example photo"></a>
-                <a href="img/example-photo-a.jpg" rel="lightbox" class="col-1"><img src="img/example-photo-a.jpg" alt="Example photo"></a>
-            </div>
-        </section>
-        <section class="row">
-            <div class="col">
-                <h2>Contact</h2>
-                <p>
-                    Want to work with us? Just send us an <a href="mailto:#">email</a>.
-                </p>
-            </div>
-            <div class="col">
-                <h2>Follow us</h2>
-                <p>
-                    We are on <a href="http://twitter.com/rickwaalders">Twitter</a>, <a href="http://dribbble.com/rickwaalders">Dribbble</a> and <a href="http://instagram.com/rickwaalders">Instagram</a>.
-                </p>
-            </div>
-        </section>
         <section class="row">
             <div class="col-full">
                 <p>
-                    © 2014 - This is a free website template by <a href="http://www.pixelsbyrick.com">Rick Waalders</a>
+                    © 2017 - PHP Experience 2017 - template by <a href="http://www.pixelsbyrick.com">Rick Waalders</a>
                 </p>
             </div>
         </section>
